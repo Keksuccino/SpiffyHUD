1bf2eb5161c7b8df76d457c7be9bfa083888f0ee
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-06-24 22:52:08 +0200

    v3.0.2 - 18

--- common/src/main/java/de/keksuccino/spiffyhud/util/rendering/GuiEntityRenderer.java
+++ common/src/main/java/de/keksuccino/spiffyhud/util/rendering/GuiEntityRenderer.java
@@ -111,10 +111,10 @@ public class GuiEntityRenderer {
         Vector3f translation = new Vector3f(0.0f, entity.getBbHeight() / 2.0f, 0.0f);
 
         // This fixes the entity being cut off at the edges
-        posX -= 10000;
-        posY -= 10000;
-        boxWidth += 20000;
-        boxHeight += 20000;
+        posX -= 1000;
+        posY -= 1000;
+        boxWidth += 2000;
+        boxHeight += 2000;
 
         // Submit the entity render state using the new 1.21.6 method
         graphics.submitEntityRenderState(
