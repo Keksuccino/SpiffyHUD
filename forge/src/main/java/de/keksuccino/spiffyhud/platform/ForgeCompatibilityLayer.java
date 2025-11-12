502b3f0a738f6178c7f7bd3c50ab629b4a52f07b
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-11-12 08:12:20 +0100

    v3.1.0 - 3

--- forge/src/main/java/de/keksuccino/spiffyhud/platform/ForgeCompatibilityLayer.java
+++
@@ -1,7 +0,0 @@
-package de.keksuccino.spiffyhud.platform;
-
-import de.keksuccino.spiffyhud.platform.services.IPlatformCompatibilityLayer;
-
-public class ForgeCompatibilityLayer implements IPlatformCompatibilityLayer {
-
-}
