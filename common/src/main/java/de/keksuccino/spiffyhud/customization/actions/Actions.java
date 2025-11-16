fd825c8ba7fc55d288dd641c451f0374ef237016
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-11-16 08:15:43 +0100

    v3.1.0 - 92

--- common/src/main/java/de/keksuccino/spiffyhud/customization/actions/Actions.java
+++ common/src/main/java/de/keksuccino/spiffyhud/customization/actions/Actions.java
@@ -2,17 +2,20 @@ package de.keksuccino.spiffyhud.customization.actions;
 
 import de.keksuccino.fancymenu.customization.action.ActionRegistry;
 import de.keksuccino.spiffyhud.customization.actions.marker.AddMarkerAction;
+import de.keksuccino.spiffyhud.customization.actions.marker.ClearMarkersAction;
 import de.keksuccino.spiffyhud.customization.actions.marker.EditMarkerAction;
 import de.keksuccino.spiffyhud.customization.actions.marker.RemoveMarkerAction;
 
 public class Actions {
 
     public static final AddMarkerAction ADD_MARKER = new AddMarkerAction();
+    public static final ClearMarkersAction CLEAR_MARKERS = new ClearMarkersAction();
     public static final EditMarkerAction EDIT_MARKER = new EditMarkerAction();
     public static final RemoveMarkerAction REMOVE_MARKER = new RemoveMarkerAction();
 
     public static void registerAll() {
         ActionRegistry.register(ADD_MARKER);
+        ActionRegistry.register(CLEAR_MARKERS);
         ActionRegistry.register(EDIT_MARKER);
         ActionRegistry.register(REMOVE_MARKER);
     }
