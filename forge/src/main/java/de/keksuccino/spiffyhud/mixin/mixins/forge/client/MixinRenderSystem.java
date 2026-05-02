a207dc76de701e450f4e71a104317262201d0021
Keksuccino <no@mail.net>
2026-05-02 15:03:51 +0200

    Added compatibility for FancyMenu v3.9.0

--- forge/src/main/java/de/keksuccino/spiffyhud/mixin/mixins/forge/client/MixinRenderSystem.java
+++ forge/src/main/java/de/keksuccino/spiffyhud/mixin/mixins/forge/client/MixinRenderSystem.java
@@ -1,4 +1,4 @@
-package de.keksuccino.spiffyhud.mixin.mixins.common.client;
+package de.keksuccino.spiffyhud.mixin.mixins.forge.client;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import de.keksuccino.spiffyhud.util.rendering.SpiffyRenderUtils;
@@ -10,13 +10,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(RenderSystem.class)
 public class MixinRenderSystem {
 
+    /**
+     * @reason Prevent blend state changes while Spiffy's blend lock is active.
+     */
     @Inject(method = "enableBlend", at = @At("HEAD"), cancellable = true)
-    private static void head_enableBlend_Spiffy(CallbackInfo info) {
+    private static void cancel_enableBlend_Spiffy(CallbackInfo info) {
         if (SpiffyRenderUtils.isBlendLocked()) info.cancel();
     }
 
+    /**
+     * @reason Prevent blend state changes while Spiffy's blend lock is active.
+     */
     @Inject(method = "disableBlend", at = @At("HEAD"), cancellable = true)
-    private static void head_disableBlend_Spiffy(CallbackInfo info) {
+    private static void cancel_disableBlend_Spiffy(CallbackInfo info) {
         if (SpiffyRenderUtils.isBlendLocked()) info.cancel();
     }
 
